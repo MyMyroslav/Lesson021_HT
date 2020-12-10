1f1d47c4f2e8ff15ee154b65a66748be449ac6e1
Myroslav <havrushkomyroslav@gmail.com>
2020-12-10 22:49:26 +0200

    Lesson021_HT DONE

---
+++ Lesson021_HT/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+
+namespace Lesson021_HT
+{
+    class MainClass
+    {
+        static object lockFlag = new Object();
+        static void Recurse(object remaining)
+        {
+            lock (lockFlag)
+            {
+                if ((int)remaining <= 0)
+                {
+                    return;
+                }
+                Thread thread = new Thread(Recurse);
+                thread.Start((int)remaining - 1);
+                Console.WriteLine(remaining);
+            }
+        }
+        public static void Main(string[] args)
+        {
+            Recurse(1000);
+        }
+    }
+}
